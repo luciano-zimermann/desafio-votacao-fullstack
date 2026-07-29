76d0e90ee3a0cab89130d45635aae59066d562a8
Luciano Zimermann <lucianozn16@gmail.com>
2026-07-29 15:46:16 -0300

    chore: Spring Boot project initial configuration

---
+++ src/main/java/com/lucianozimermann/desafiovotacaofullstack/DesafioVotacaoFullstackApplication.java
@@ -0,0 +1,13 @@
+package com.lucianozimermann.desafiovotacaofullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioVotacaoFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioVotacaoFullstackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucianozimermann/desafiovotacaofullstack/DesafioVotacaoFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucianozimermann.desafiovotacaofullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiovotacaofullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
