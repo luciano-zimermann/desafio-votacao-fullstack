998236502fe15fd67af2bd6d24073f236dead128
Luciano Zimermann <lucianozn16@gmail.com>
2026-08-02 17:55:56 -0300

    docs: add swagger documentation

---
+++ backend/src/main/java/com/lucianozimermann/desafiovotacaofullstack/controller/api/v1/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.lucianozimermann.desafiovotacaofullstack.controller.api.v1;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(new Info()
+                                          .title("Desafio Votação Fullstack")
+                                          .version("1.0.0")
+                                          .description("Documentação automática da API - Desafio Votação Fullstack")
+                                          .contact(new Contact().name("Luciano Zimermann")
+                                                                .email("lucianozn16@gmail.com")));
+    }
+}
